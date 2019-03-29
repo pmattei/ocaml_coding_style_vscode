b52507729cae8e659ce9c75df0e9d6a314c45c33
pmattei <philippe.mattei@lexifi.com>
2019-03-29 14:53:00 +0100

    fix end line behavior for : and add correct space for ( ) and { }

--- extension.js
+++ extension.js
@@ -8,22 +8,50 @@ function run() {
 	//let wholeRange = new Range (startPosition, endPositionLastRange);
 	//let wholeText = document.getText(wholeRange)
 
-	// text for test azae:aaz azea: zae azeaz :zae eaz:  zeazea azeaz  :zae
-	// text for test azae:aaz azea:	zae azeaz	:zae eaz:		zeazea azeaz		:zae
+	// text for test azae:aaz azea: zae azeaz :zae eaz:   zeazea azeaz   :zae
+	// text for test azae:aaz azea:	zae azeaz	:zae eaz:			zeazea azeaz			:zae
 	// text for test azae::aaz azea::  zae azea  ::zae azea:  :  zae azea  :  :zae
 	// text for test azae::aaz azea::		zae azea		::zae azea:		:		zae azea		:		:zae
 	// text for test azae:=aaz azea:=  zae azea  :=zae azea:  =  zae azea  :  =zae
 	// text for test azae:=aaz azea:=		zae azea		:=zae azea:		=		zae azea		:		=zae
 
+	// text for test azae:
+	// aze
+
+	// text for test (    )  (a )  ( a) (a  )  (  a) ( 	(	 ( 	( 	a 	)	 ) 	) 	)
+	// text for test (
+	//	 	 a
+	// 	 )
+	let text_parenthese = ` ( (
+		activate
+		 ) )
+		` ;
+		let text_parenthese2 = ` (	(
+			activate
+			azeazc	 )
+			 )
+			` ;
+
+	// text for test {    }  {a }  { a} {a  }  {  a} { 	{	 { 	{ 	a 	}	 } 	} 	}
+	// text for test {
+	//	 	 a
+	// 	 }
+
 	let selection = editor.selection
 	let text = editor.document.getText(selection)
 	//vscode.window.showInformationMessage("selection : " + text);
-	let textreg = text.replace(new RegExp(/(\s|\t)*[\:](\s|\t)*/g,'gi'),": ")
-	//vscode.window.showInformationMessage("selection 2 : " + textreg);
-	textreg = textreg.replace(new RegExp(/[\:]\s[\:]\s/g,'gi')," :: ")
+	textreg = text.replace(new RegExp(/(\s)*[\:][^\S\n\r]*/g,'g'),": ")
 	//vscode.window.showInformationMessage("selection 3 : " + textreg);
-	textreg = textreg.replace(new RegExp(/[\:]\s[\=](\s|\t)*/g,'gi')," := ")
+	textreg = textreg.replace(new RegExp(/[\:]\s[\:]\s/g,'g')," :: ")
 	//vscode.window.showInformationMessage("selection 4 : " + textreg);
+	textreg = textreg.replace(new RegExp(/[\:]\s[\=][^\S\n\r]*/g,'g')," := ")
+	//vscode.window.showInformationMessage("selection 5 : " + textreg);
+	textreg = textreg.replace(new RegExp(/\([^\S\n\r]*/g,'g'),"(")
+	textreg = textreg.replace(new RegExp(/([\S])[^\S\n\r]+\)/g,'g'),"$1)")
+	textreg = textreg.replace(new RegExp(/([\S])[^\S\n\r]+\)/g,'g'),"$1)")
+	textreg = textreg.replace(new RegExp(/\{[^\S\n\r]*/g,'g'),"{")
+	textreg = textreg.replace(new RegExp(/([\S])[^\S\n\r]+\}/g,'g'),"$1}")
+	textreg = textreg.replace(new RegExp(/([\S])[^\S\n\r]+\}/g,'g'),"$1}")
 	editor.edit(builder => builder.replace(selection, textreg));
 }
 
