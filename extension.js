edd7af8ef13ad1d5856d0f65119c9a793c5630a2
pmattei <philippe.mattei@lexifi.com>
2019-03-29 10:28:08 +0100

    fix for :: separator

--- extension.js
+++ extension.js
@@ -8,12 +8,18 @@ function run() {
 	//let wholeRange = new Range (startPosition, endPositionLastRange);
 	//let wholeText = document.getText(wholeRange)
 
+	// text for test azae:aaz azea: zae azeaz :zae eaz:  zeazea azeaz  :zae
+	// text for test azae:aaz azea:	zae azeaz	:zae eaz:		zeazea azeaz		:zae
+	// text for test azae::aaz azea::  zae azea  ::zae azea:  :  zae azea  :  :zae
+	// text for test azae::aaz azea::		zae azea		::zae azea:		:		zae azea		:		:zae
 
 	let selection = editor.selection
 	let text = editor.document.getText(selection)
 	//vscode.window.showInformationMessage("selection : " + text);
 	let textreg = text.replace(new RegExp(/(\s|\t)*[\:](\s|\t)*/g,'gi'),": ")
-	//vscode.window.showInformationMessage(`textreg: ${textreg}`);
+	//vscode.window.showInformationMessage("selection 2 : " + textreg);
+	textreg = textreg.replace(new RegExp(/[\:]\s[\:]\s/g,'gi')," :: ")
+	//vscode.window.showInformationMessage("selection 2 : " + textreg);
 	editor.edit(builder => builder.replace(selection, textreg));
 }
 
