a9b6faef27df585d9c79c9d937373322484c8410
pmattei <philippe.mattei@lexifi.com>
2019-04-01 09:59:34 +0200

    nouvelle prise en compte des : en particulier dans les type et les val

--- extension.js
+++ extension.js
@@ -2,23 +2,55 @@ const vscode = require('vscode');
 
 function run() {
 	let editor = vscode.window.activeTextEditor;
+	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
 	//let document = editor.document;
 	//let endPositionLastRange = document.lineAt(document.lineCount).range.end;
 	//let startPosition = new vscode.Position(0,0);
 	//let wholeRange = new Range (startPosition, endPositionLastRange);
 	//let wholeText = document.getText(wholeRange)
 
-	// text for test azae:aaz azea: zae azeaz :zae eaz:   zeazea azeaz   :zae
-	// text for test azae:aaz azea:	zae azeaz	:zae eaz:			zeazea azeaz			:zae
+	// text for test val 	fun 	: 	x val 	fun:x
+	// text for test val 	fun 	:
+	// x
+	// text for test ({az}:aa) (aze: zae) (eaz :zae) (aze:	zae) (eaz	:zae) (eaz: 	 zeazea) (azeaz	  :zae)
+	let text_typage_with_chariot_return_do_not_change = `(	 x:
+		y	 )`;
+	// text for test ({(a:z);_}:aa)  ({a : z;_}:aa) ({a: 	z;_}: 	aa) ({a 	:z;_} 	:aa) ({a 	:		 z;_} 	: 	 	aa)
+	// type t = {foo: int; bar: float}
+	//    let f (x:t):unit =
+	//    let {foo=(a:int);_}:t = (x:t) in
+	//    let {foo=(a 	: 	int);_} 	:	 t = (x 	: 	t) in
+	//    let ( 	{ 	foo 	= 	(a 	: 	int) 	; 	_ 	} 	: 	t 	) = ( 	x 	: 	t 	) in
+	//    ()
+	// objectif
+	// type t = {foo: int; bar: float}
+	//    let f (x : t) : unit =
+	//    let {foo=(a : int);_} : t = (x : t) in
+	//    let {foo=(a : int);_} : t = (x : t) in
+	//    let ({ 	foo 	= 	(a : int) 	; 	_ 	} : t) = (x : t) in
+	//    ()
+	// text for test type aze = {azez : vlk ;  qsds : blabla}
+	// text for test type aze = {azez 	: vlk ;  qsds	:	 blabla;}
+	// text for test type aze = {azez 	: vlk; qsds	: blabla ; }
+	// text for test type aze = {aaa : rrr ;  bbb : sss; ccc : tttt}
+	// text for test type aze = {aaa : rrr ;  bbb : sss; ccc : tttt; ddd : uuuuu}
+	let text_type = ` type azeaz =
+	{
+		aaa : rrr ;
+		bbb : sss;
+		ccc : tttt;
+		ddd : uuuuu
+	}`
+	// text for test let x = f ~x:y ()  (* no whitespace *)
+	// text for test azae:
+	// aze
 	// text for test azae::aaz azea::  zae azea  ::zae azea:  :  zae azea  :  :zae
 	// text for test azae::aaz azea::		zae azea		::zae azea:		:		zae azea		:		:zae
 	// text for test azae:=aaz azea:=  zae azea  :=zae azea:  =  zae azea  :  =zae
 	// text for test azae:=aaz azea:=		zae azea		:=zae azea:		=		zae azea		:		=zae
 
-	// text for test azae:
-	// aze
 
-	// text for test (    )  (a )  ( a) (a  )  (  a) ( 	(	 ( 	( 	a 	)	 ) 	) 	)
+	// text for test (    )  (a )  ( a) (a  )  (  a) ( 	(	 ( 	( 	a 	)	 ) 	) 	) ( : )
 	// text for test (
 	//	 	 a
 	// 	 )
@@ -40,12 +72,28 @@ function run() {
 	let selection = editor.selection
 	let text = editor.document.getText(selection)
 	//vscode.window.showInformationMessage("selection : " + text);
-	textreg = text.replace(new RegExp(/(\s)*[\:][^\S\n\r]*/g,'g'),": ")
+	textreg = text.replace(new RegExp(/[^\S\n\r]*\:[^\S\n\r]*/g,'g')," : ")
+	//textreg = text.replace(new RegExp(/([^\s\:]+( : [^\s\:])*)[^\S\n\r]*[\:][^\S\n\r]*([^\s\:]+)/g,'g'),"$1 : $3")
+	//vscode.window.showInformationMessage("selection 2 : " + textreg);
+	textreg = textreg.replace(new RegExp(/val[^\S\n\r]+([^\s\:]+) \: /g,'g'),"val $1: ")
 	//vscode.window.showInformationMessage("selection 3 : " + textreg);
-	textreg = textreg.replace(new RegExp(/[\:]\s[\:]\s/g,'g')," :: ")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{[\s]*)([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{[\s]*[\S]+\: [\S]+[\s]*;[\s]*)([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){2})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){3})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){4})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){5})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){6})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){7})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){8})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
+	textreg = textreg.replace(new RegExp(/(?<=type[\s]+[\S]+[\s]+=[\s]+{(?:[\s]*[\S]+\: [\S]+[\s]*;[\s]*){9})([\S]+) \: ([\S]+)/g,'g'),"$1: $2")
 	//vscode.window.showInformationMessage("selection 4 : " + textreg);
-	textreg = textreg.replace(new RegExp(/[\:]\s[\=][^\S\n\r]*/g,'g')," := ")
+	textreg = textreg.replace(new RegExp(/\~([\S]+) \: /g,'g'),"\~$1:")
 	//vscode.window.showInformationMessage("selection 5 : " + textreg);
+	textreg = textreg.replace(new RegExp(/[\:]\s[\:]\s/g,'g')," :: ")
+	//vscode.window.showInformationMessage("selection 6 : " + textreg);
+	textreg = textreg.replace(new RegExp(/[\:]\s[\=][^\S\n\r]*/g,'g')," := ")
+	//vscode.window.showInformationMessage("selection 7 : " + textreg);
 	textreg = textreg.replace(new RegExp(/\([^\S\n\r]*/g,'g'),"(")
 	textreg = textreg.replace(new RegExp(/([\S])[^\S\n\r]+\)/g,'g'),"$1)")
 	textreg = textreg.replace(new RegExp(/([\S])[^\S\n\r]+\)/g,'g'),"$1)")
